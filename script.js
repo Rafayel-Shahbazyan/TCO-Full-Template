830d69ed2bc8e54a7c1cc38ac8449f8816af2118
Your Name <you@example.com>
2020-07-07 20:06:26 +0400

    TCO Full Template

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('open-menu').addEventListener('click' , () => {
+    document.getElementById('nav').classList.toggle('open-menu');
+})
+document.getElementById('sendButton').addEventListener('click' , () => {
+    document.getElementById('span1').style.opacity = '1'
+    document.getElementById('span2').style.opacity = '1'
+    document.getElementById('span3').style.opacity = '1'
+})
